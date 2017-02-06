8f6e773dc6bedef664a6c8b723416e67a9825707
Fei Peng <felix@dyn-160-39-196-229.dyn.columbia.edu>
2017-02-06 11:50:10 -0500

    complete charge service and modify paySuccess page

--- payment.js
+++ payment.js
@@ -1,15 +1,17 @@
 'use strict'
-const keyPublishable = process.env.PUBLISHABLE_KEY;
-const keySecret = process.env.SECRET_KEY;
 
-var express = require("express");
-const stripe = require("stripe")(keySecret);
-var path    = require("path");
-//var bodyParser = require("body-parser");
+const keyPublishable = 'pk_test_1zX2sGaxbQYs7Ke0IpcCan3V';
+const keySecret = 'sk_test_Q4JWUjA2m8GfpIUZy4nHncmt';
+
+var express = require('express');
+var stripe = require('stripe')(keySecret);
+var path    = require('path');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var app = express();
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'/templates/login.html'));
@@ -38,19 +40,39 @@ app.get('/list', function(req, res){
 app.post('/charge',function(req, res){
   var token = req.body.stripeToken;
   var chargeAmount = req.body.chargeAmount;
+  var description = req.body.description;
   var charge = stripe.charges.create({
     amount: chargeAmount,
-    currency: "USD",
+    currency: "usd",
     source: token
   }, function(err, charge){
-    if(err & err.type == "StripeCardError"){
-      console.log("Your card is rejected.");
+    if(err){
+      if(err.type == "StripeCardError"){
+        console.log("Your card is rejected.");
+      }
+    }else{
+      console.log("Success!")
+
+      //update Payment records here
+      var connection = mysql.createConnection({
+        host     : 'http://modernservice.c4mnz6jezil1.us-east-1.rds.amazonaws.com',
+        user     : 'ModernService',
+        password : 'ModernService',
+        port     : '3306'
+      });
+
+      connection.connect();
+
+      connection.query("insert into payments('Description', 'Created','Amount','Currency') values (?, ?, ? ,'USD')", [description, new Date(),chargeAmount], function (error, results, fields) {
+        if (error) throw error;
+        console.log("Payment record has been pushed to database.");
+      });
+
+      connection.end();
+
+      res.redirect('/paySuccess');
     }
   });
-  console.log("Success!")
-  //update Payment records here
-
-  res.redirect('/paySuccess');
 });
 
 app.get('/paySuccess', function(req, res){
