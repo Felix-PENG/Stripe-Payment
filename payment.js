560591ede71785cfbb256eab77a53cf2602f58d6
Fei Peng <felix@dyn-160-39-196-71.dyn.columbia.edu>
2017-02-05 19:44:32 -0500

    modify backend charge service

--- payment.js
+++ payment.js
@@ -2,27 +2,28 @@
 const keyPublishable = process.env.PUBLISHABLE_KEY;
 const keySecret = process.env.SECRET_KEY;
 
-const app = require("express")();
+var express = require("express");
 const stripe = require("stripe")(keySecret);
 var path    = require("path");
-var express = require('express');
+var bodyParser = require("body-parser");
 
-app.use(express.static('public'));
-// app.set('view engine', 'pug')
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.get("/", function(req, res){
+app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'/templates/login.html'));
 });
 
-app.get("/signup", function(req, res){
+app.get('/signup', function(req, res){
   res.sendFile(path.join(__dirname+'/templates/signup.html'));
 });
 
-app.get("/charge", function(req, res){
+app.get('/charge', function(req, res){
   res.sendFile(path.join(__dirname+'/templates/charge.html'));
 });
 
-app.get("/list", function(req, res){
+app.get('/list', function(req, res){
   res.sendFile(path.join(__dirname+'/templates/list.html'));
 });
 // =======
@@ -34,28 +35,26 @@ app.get("/list", function(req, res){
 //   res.render("index.html", {keyPublishable}));
 // >>>>>>> 59cd8462f9aa38e5953c797d47654b6729733980
 
-app.post("/charge", (req, res) => {
-  let amount = 500;
-
-  stripe.customers.create({
-     email: req.body.stripeEmail,
-    source: req.body.stripeToken
-  })
-  .then(customer =>
-    stripe.charges.create({
-      amount,
-      description: "Sample Charge",
-         currency: "usd",
-         customer: customer.id
-    }))
-  .then(charge => res.render("charge.pug"));
+app.post('/charge',function(req, res){
+  var token = req.body.stripeToken;
+  var chargeAmount = req.body.chargeAmount;
+  var charge = stripe.charges.create({
+    amount: chargeAmount,
+    currency: "USD",
+    source: token
+  }, function(err, charge){
+    if(err & err.type == "StripeCardError"){
+      console.log("Your card is rejected.");
+    }
+  });
+  res.redirect('/paySuccess');
 });
 
-app.get("/paySuccess", function(req, res){
+app.get('/paySuccess', function(req, res){
   res.sendFile(path.join(__dirname+'/templates/paySuccess.html'));
 });
 
-app.get("/updatePayment", function(req, res){
+app.get('/updatePayment', function(req, res){
 
 });
 
